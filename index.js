5d6b90dcf20d0001f45d0f313a74ec6729dfc4d0
travis-konarik <travis_konarik@outlook.com>
2020-01-18 21:00:33 -0500

    Adding env

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const request = require("request");
 
+console.log(process.env.API_TEST_APP_A_ROOT + "/");
+
 request(process.env.API_TEST_APP_A_ROOT + "/", function (error, response, body) {
         if (error) {
             console.log('Something Went Wrong');
